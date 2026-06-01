9a2356db82ad61b245c1e3772e537647aee76b86
safiraedigital <safiraedigital@gmail.com>
2026-06-01 13:35:54 -0300

    Fix Vercel build entrypoint

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "./",
-  build: {
-    rollupOptions: {
-      input: "app.html"
-    }
-  },
   plugins: [react()]
 });
